543aa337ad2687d2b1e8193c8b3d67c2ac35612c
Jason Bothel <shadowmanguyyt@gmail.com>
2018-10-09 23:43:52 -0400

    Quick test to switch to ws

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import * as express from 'express'
 import { readFileSync } from 'fs'
 import * as https from 'https'
 import * as socketio from 'socket.io'
+import * as Websocket from 'ws'
 import * as path from 'path'
 
 const app = express()
@@ -11,25 +12,25 @@ const server = https.createServer({
 }, app).listen(3000, function () {
   console.log(`Listening on port 3000`)
 })
-const io = socketio(server)
+const wss = new Websocket.Server({ server })
 
 app.get('/', function (req, res) {
   res.sendFile(path.resolve('public', 'index.html'))
 })
 
-io.on('connection', function (socket) {
-  console.log('A user connected: ' + socket.client.conn.remoteAddress)
+wss.on('connection', function (ws, req) {
+  console.log('A user connected: ' + req.connection.remoteAddress)
 
-  socket.on('disconnect', () => {
-    console.log('A user disconnected: ' + socket.client.conn.remoteAddress)
+  ws.on('disconnect', () => {
+    console.log('A user disconnected: ' + req.connection.remoteAddress)
   })
 
-  socket.on('chatMessage', (message: string) => {
+  ws.on('chatMessage', (message: string) => {
     if (message === '') {
       return
     }
 
-    console.log(`New message from ${socket.client.conn.remoteAddress}: ${message}`)
-    io.emit('chatMessage', message)
+    console.log(`New message from ${req.connection.remoteAddress}: ${message}`)
+    wss.emit('chatMessage', message)
   })
 })
